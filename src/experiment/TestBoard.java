cb2bcdd1d03a1c1e31ffcd3c5ed525c1aa5105a1
Cade Shiveley <cade_shiveley@mines.edu>
2025-10-13 19:57:15 -0600

    check

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,28 @@
 package experiment;
+import TestBoardCell;
+
 
 public class TestBoard {
+private int pathlength;
+private int startCell;
+private int row;
+private int column;
+Set<TestBoardCell> getTargets;
+private int targets;
+
+public void calcTargets(TestBoardCell startCell, int pathlength) {
+	
+}
+
+public TestBoardCell getCell(int row, int column) {
+	return this.row;
+	return this.column;
+}
+
+public Set<TestBoardCell> targets(){
+	return targets;
+	
+}
+
 
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,47 @@
 package experiment;
 
 public class TestBoardCell {
+	private int row;
+	private int column;
+	private Set<TestBoardCell> adjList;
+	private boolean isRoom;
+	private boolean isOccupied;
+	
+	public void addAdjacency (TestBoardCell cell) {
+		adjList.add(cell);
+	}
+	
+	public Set<TestBoardCell> getAdjList(){
+		return adjList;
+	}
+	
+	public TestBoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
 
+
+	public boolean isRoom() {
+		return isRoom;
+	}
+
+	public void setRoom(boolean isRoom) {
+		this.isRoom = isRoom;
+	}
+
+	public boolean isOccupied() {
+		return isOccupied;
+	}
+
+	public void setOccupied(boolean isOccupied) {
+		this.isOccupied = isOccupied;
+	}
+
+
+	
+	
+	
+	
+	
 }
