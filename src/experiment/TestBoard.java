a1508a0dc445dae8e8215a558674c7f8c5607d5e
Ben Roan <baroan@mines.edu>
2025-10-20 14:32:07 -0600

    Added calcAdjacencies method and tests passing

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -4,62 +4,87 @@ import java.util.Set;
 
 public class TestBoard {
 	//initializes
-//sets targets
-Set<TestBoardCell> targets;
-private TestBoardCell[][] grid;
-private Set<TestBoardCell> visited;
-final static int COLS =4;
-final static int ROWS = 4;
-
-//assistance from TA Thomas
-
-
-public void board() {
-
-}
-
-private void findAllTargets(TestBoardCell thisCell, int numSteps) {
-	for(TestBoardCell adjCell:  adjList(thisCell)) {
-		//check if visited skip rest
-		if (visited.contains(thisCell)==true){
-			return;
-			
-		}
-		visited.add(thisCell);
-		
+	//sets targets
+	Set<TestBoardCell> targets;
+	private TestBoardCell[][] grid;
+	private Set<TestBoardCell> visited;
+	final static int COLS = 4;
+	final static int ROWS = 4;
 	
-		if (numSteps == 1) {
-			targets.add(thisCell);
-		}else {
-			findAllTargets(adjCell, numSteps -1);
+	//assistance from TA Thomas
+	
+	
+	public TestBoard() {
+		grid = new TestBoardCell[ROWS][COLS];
+		targets = new HashSet<>();
+		visited = new HashSet<>();
+		
+		for (int r = 0; r < ROWS; r++) {
+			for (int c = 0; c < COLS; c++) {
+				grid[r][c] = new TestBoardCell(r, c);
+			}
 		}
-		//stack overflow for syntax how to remove stuff from setlist
-		visited.remove(thisCell);
+		
+		calcAdjacencies();
+		
 	}
 	
+	private void findAllTargets(TestBoardCell thisCell, int numSteps) {
+		for(TestBoardCell adjCell:  thisCell.getAdjList()) {
+			//check if visited skip rest
+			if (visited.contains(adjCell) || adjCell.isOccupied()){
+				continue;
+			}
+			
+			visited.add(adjCell);
+			
+		
+			if (numSteps == 1 || adjCell.isRoom()) {
+				targets.add(adjCell);
+			} else {
+				findAllTargets(adjCell, numSteps-1);
+			}
+			//stack overflow for syntax how to remove stuff from setlist
+			visited.remove(adjCell);
+		}
+		
+		
+	}
 	
-}
-
-//calculates the cells a player can go to
-public void calcTargets(TestBoardCell startCell, int pathlength) {
-	//call findAllTargets in this - done
-	//make sure visited list is empty and targets
-	//add start cell to visited list
+	public void calcAdjacencies() {
+		for (int r = 0; r < ROWS; r++) {
+			for (int c = 0; c < COLS; c++) {
+				TestBoardCell cell = grid[r][c];
+				
+				if (r > 0) cell.addAdjacency(grid[r-1][c]);
+				if (r < ROWS-1) cell.addAdjacency(grid[r+1][c]);
+				if (c > 0) cell.addAdjacency(grid[r][c-1]);
+				if (c < COLS-1) cell.addAdjacency(grid[r][c+1]);
+				
+			}
+		}
+	}
 	
-	visited.add(startCell);
-	findAllTargets(startCell, pathlength);
-	visited.remove(pathlength);
-}
-
-//returns current cell 
-public TestBoardCell getCell(int row, int column) {
-	return grid[row][column];
-}
-
-//returns the list of targets that were found in calc
-public Set<TestBoardCell> targets(){
-	return targets;
-}
+	//calculates the cells a player can go to
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		//call findAllTargets in this - done
+		//make sure visited list is empty and targets
+		//add start cell to visited list
+		
+		targets = new HashSet<>();
+		visited = new HashSet<>();
+		findAllTargets(startCell, pathlength);
+	}
+	
+	//returns current cell 
+	public TestBoardCell getCell(int row, int column) {
+		return grid[row][column];
+	}
+	
+	//returns the list of targets that were found in calc
+	public Set<TestBoardCell> targets(){
+		return targets;
+	}
 
 
 }
