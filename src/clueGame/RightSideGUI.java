fc9d52552eedc5ff5854d9cb3570ba41dd2fa2e2
Cade Shiveley <cade_shiveley@mines.edu>
2025-11-24 08:44:03 -0700

    adding the right side panel

---
+++ src/clueGame/RightSideGUI.java
@@ -0,0 +1,104 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class RightSideGUI extends JFrame {
+	public JPanel rightSideGUI() {
+		JPanel panel = new JPanel();
+		panel.add(KnownCards());
+		return panel;
+		
+	}
+	
+	public RightSideGUI() {
+		setTitle("right side gui");
+		setSize(100,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(rightSideGUI());
+		setVisible(true);
+	}
+	
+	public JPanel rightsidegui() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1,1));
+		panel.add(KnownCards());
+		return panel;
+		
+	}
+	private JPanel KnownCards() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(), ("Known Cards)")));
+		panel.setLayout(new GridLayout(3,1));
+		
+		panel.add(people());
+		panel.add(rooms());
+		panel.add(weapons());
+		
+		return panel;
+		
+		
+	}
+	
+	private JPanel people() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(), ("People")));
+		
+		JLabel peopleInHand = new JLabel("In Hand: ");
+		JLabel peopleSeen = new JLabel("Seen: ");
+
+		
+		panel.add(peopleInHand);
+		panel.add(peopleSeen);
+		
+		return panel;
+	}
+	
+	private JPanel rooms() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(), ("Rooms")));
+		
+		JLabel roomsInHand = new JLabel("In Hand: ");
+		JLabel roomsSeen = new JLabel("Seen: ");
+		
+
+		panel.add(roomsInHand);
+		panel.add(roomsSeen);
+		
+		return panel;
+	}
+	
+	private JPanel weapons() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(), ("Weapons")));
+		
+		JLabel weaponsInHand = new JLabel("In Hand: ");
+		JLabel weaponsSeen = new JLabel("Seen: ");
+
+
+
+		panel.add(weaponsInHand);
+		panel.add(weaponsSeen);
+		
+		return panel;
+	}
+	
+	public static void main(String[] args) {
+		new RightSideGUI();
+
+		
+	}
+	
+	
+}
